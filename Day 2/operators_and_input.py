079e7e5f451164cee82217374bd6069c8721d472
Hamadbuilds <hamadhms07@gmail.com>
2025-12-25 10:56:51 +0530

    Day 2: basic operators and input

---
+++ Day 2/operators_and_input.py
@@ -0,0 +1,32 @@
+# Day 2: Basic Operators and Input
+
+# Arithmetic Operators
+a = 10
+b = 3
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Modulus:", a % b)
+print("Exponent:", a ** b)
+
+print("--------------------")
+
+# Comparison Operators
+x = 5
+y = 8
+
+print("x == y:", x == y)
+print("x != y:", x != y)
+print("x > y:", x > y)
+print("x < y:", x < y)
+
+print("--------------------")
+
+# Input from user
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print("Hello", name)
+print("You will be", age + 1, "years old next year")
