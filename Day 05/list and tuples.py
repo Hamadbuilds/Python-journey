44c813040a7efe58f644d07f90a9cef1f9ce923d
Hamadbuilds <hamadhms07@gmail.com>
2025-12-28 17:30:03 +0530

    Day 5: Create list and tuples

---
+++ Day 05/list and tuples.py
@@ -0,0 +1,15 @@
+#list
+
+fruits=['apple','pear',3]
+print(fruits)
+print(fruits[0])
+
+fruits.append('strawberry')
+print(fruits)
+
+fruits[2]='blueberry'
+print(fruits)
+
+#tuple
+colour=(255,255,255)
+print(type(colour))
