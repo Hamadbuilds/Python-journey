9be7378a8c9c205b043fd2477e436e764740a142
Hamadbuilds <hamadhms07@gmail.com>
2026-01-03 22:34:45 +0530

    Day 08: Slice operator

---
+++ Day 08/Slice operator.py
@@ -0,0 +1,15 @@
+fruits = ['apples','pear','strawberry']
+text='Hello I like Python'
+
+print(text[0:4])
+
+print(text[2:])
+
+print(text[::2])
+
+print(text[3::3])
+
+print(fruits[1:])
+
+fruits[1:1]='b'
+print(fruits)
