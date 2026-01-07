931915a7e962793380e1180afece4fb58fb37ee9
Hamadbuilds <hamadhms07@gmail.com>
2026-01-07 19:24:42 +0530

    Day 09: Functions

---
+++ Day 09/Functions.py
@@ -0,0 +1,27 @@
+def addTwo(x):
+    return x+2
+
+def subTwo(number):
+    return number-2
+
+a=addTwo(7)
+print(a)
+
+def printstring(string):
+    print(string)
+
+printstring('Hello')
+
+def accel(mass,force):
+    a= mass*force
+    return a
+
+print(accel(2,5))
+
+def hi():
+    print('hi')
+
+hi()
+
+
+
