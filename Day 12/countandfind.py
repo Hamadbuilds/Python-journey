52d30282dd466607944f9c3eb7904c7fd635516c
Hamadbuilds <hamadhms07@gmail.com>
2026-01-10 14:45:23 +0530

    Day 12:countandfind

---
+++ Day 12/countandfind.py
@@ -0,0 +1,18 @@
+# .find(), .count()
+a='helloskmdlkdsjiodshaoh'
+print(a.find('l'))
+print(a.find('o'))
+print(a.find('7'))
+print(a.count('l'))
+print(a.count('sh'))
+if a.find('l')== 2:
+    print('wow')
+else:
+    print('no')
+
+b=input('Type smthg:')
+if b.count('_')>0:
+    print('bad')
+else:
+    print('good')
+
